4db8837ec087f63fef97525ee5913fb684eed034
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-20 18:58:55 +0100

    initialize welcome component

---
+++ src/pages/welcome/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Heading, Box, Container, Flex, Icon, Image, VStack, HStack , Button, Text, Link } from '@chakra-ui/react'
+
+const Welcome = ()=> {
+    return (
+        <Box>
+            
+        </Box>
+    )
+}
+
+
+
+export default Welcome;
