72576548c80dae9b26ef09835d5d6e24031711f2
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-20 23:38:13 +0100

    add heading and caption to the right side of the welcome page box

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -3,11 +3,33 @@ import { Heading, Box, Container, Flex, Icon, Image, VStack, HStack , Button, Te
 
 const Welcome = ()=> {
     return (
-        <Box>
+        <Box display={{md: "flex", sm: "block"}} width="full" height="100vh">
+            {/* Left Box */}
+            <Box width={{md: "40%", sm: "100%"}} h="full"  bg="primary.100">
+
+            </Box>
+
+
+            {/* Right Box */}
+            <Box display="flex" justifyContent="center" alignItems="center" width={{md: "60%", sm: "100%"}} h="full" bg="#fff">
+
+                <Container>
+
+                    <Heading>Welcome Allison,</Heading>
+                    <p>Thank you for signing up to Hidea Africa</p>
+
+                </Container>
+
+            </Box>
+            
             
         </Box>
     )
 }
+
+
+
+
 
 
 
