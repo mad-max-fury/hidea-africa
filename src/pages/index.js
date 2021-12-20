8ec28cfc1d8651bbdfbeb8b6e85046fc621b1e5b
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-20 18:59:39 +0100

    import welcome component into the pages' index file and export for global access

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Welcome from "./welcome";
+// import SignUp from "./signup";
+// import Login from "./login";
+// import Home from "./home";
+// import ComingSoon from "./comingsoon"
+
+// export {  Welcome, SignUp, Login, Home, ComingSoon}
+
+export {  Welcome }
