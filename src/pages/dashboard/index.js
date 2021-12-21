a3857c37ce0e6766df20925d6a2da9b12851cf11
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 03:16:43 +0100

    add sidebar/side-nav to dashboard page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -4,12 +4,14 @@ import Header from './components/Header'
 import "./Dashboard.css"
 
 import React from 'react'
+import SideNav from './components/SideNav'
 
 const Dashboard = ()=> {
     return (
         <Box>
 
             <Header />
+            <SideNav />
 
         </Box>
     )
