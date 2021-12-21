2fc0216f79594b0435b098e6d4416b6b15a7e5bf
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 02:29:24 +0100

    create user avatar component

--- src/pages/dashboard/components/Header.js
+++ src/pages/dashboard/components/Header.js
@@ -1,11 +1,31 @@
-import { Box } from '@chakra-ui/react'
+import { Avatar, AvatarBadge, Box, HStack, Icon, Image, InputAddon, Switch, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const Header = ()=> {
     return (
-        <header>
-            
+        <header className="fixed flex flex-row place-content-end align-middle shadow-md w-full h-20 px-16">
+            <HStack>
+                <Text fontWeight="700">SC</Text>
+                <Switch />
+                <Text fontWeight="700">NGN</Text>
+                <Icon name="notification" />
+
+                <UserAvatar avatar={ user } name="Alison Uwa" />
+            </HStack>
         </header>
+    )
+}
+
+
+//Avatar component
+const UserAvatar = ({ avatar, name }) => {
+    return (
+        <VStack>
+            <Avatar src={ avatar }>
+                <AvatarBadge boxSize='1.25em' bg='green.500' />
+            </Avatar>
+            <Text>{ name }</Text>
+        </VStack>
     )
 }
 
