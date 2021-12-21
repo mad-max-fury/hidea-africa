727fd3512acbc5db2cf6d5eb56e95bf29ffde061
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 01:53:01 +0100

    add chakra-ui Box component to dashboard page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,10 +1,11 @@
+import { Box, Icon } from '@chakra-ui/react'
 import React from 'react'
 
 const Dashboard = ()=> {
     return (
-        <div>
+        <Box>
             
-        </div>
+        </Box>
     )
 }
 
