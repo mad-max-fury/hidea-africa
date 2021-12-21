1e4fecc1c065f8cac08c26d5f70ac0abe7e3ab0c
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 17:52:30 +0100

    wrap dashboard with flex component

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,19 +1,29 @@
-import { Box, Icon } from '@chakra-ui/react'
+import { Box, Flex, Icon, VStack } from '@chakra-ui/react'
 import Header from './components/Header'
 
 import "./Dashboard.css"
 
 import React from 'react'
 import SideNav from './components/SideNav'
+import Home from './screens/Home'
 
 const Dashboard = ()=> {
     return (
-        <Box>
 
-            <Header />
-            <SideNav />
+            <Flex width="full" height="100vh" direction="row">
+                <SideNav />
+                <VStack width="full" bg="background.100">
+                    <Header />
+                    {/* Dashboard screens */}
+                    <main className="w-full h-full p-8 main-container">
 
-        </Box>
+                        <Home />
+
+                        
+
+                    </main>
+                </VStack>
+            </Flex>
     )
 }
 
