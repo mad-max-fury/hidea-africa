89e9ee1b6ee71b68f4234a10c3fbb33b7fc7082b
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 15:46:10 +0100

    create left side of home screen component

--- src/pages/dashboard/screens/Home.js
+++ src/pages/dashboard/screens/Home.js
@@ -1,15 +1,42 @@
-import { Box, Flex, Heading, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Text, VStack, Spacer } from '@chakra-ui/react'
 import React from 'react'
 
 const Home = ()=> {
+
     return (
         <Flex>
 
-            <VStack>
+            <VStack alignItems="flex-start">
 
                 <Text fontWeight="700" fontSize="24px">Welcome Allison,</Text>
-                <Box>
-                    
+                <Box display="flex" flexDirection="row" mt="8" mb="8" padding="16px" pb="6" h="fit-content" width="fit-content" bg="white" borderRadius="16px" w="704px">
+                    <Box alignItems="flex-start">
+                        <p>Total investments</p>
+                        <Heading>SC 42,000,000</Heading>
+
+                        {/* Percentage reading */}
+                        <Flex w="full" justifyContent="space-between" mt="8">
+                            <VStack alignItems="flex-start" flex="1">
+                                <p className="text-xs">Total equity</p>
+                                <Text fontWeight="700">10%</Text>
+                            </VStack>
+
+                            <VStack alignItems="flex-start" flex="1">
+                                <p className="text-xs">Total interest</p>
+                                <Text fontWeight="700">25%</Text>
+                            </VStack>
+                        </Flex>
+
+                        <Button bg="secondary.100" color="white" w="90%" h="48px" mt="56px">View report</Button>
+                    </Box>
+
+
+                    {/* Graph section */}
+                    <Box>
+                        
+                    </Box>
+
+
                 </Box>
 
             </VStack>
