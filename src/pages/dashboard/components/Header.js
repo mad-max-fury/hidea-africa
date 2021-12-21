db4c63153c2abc4f50f37442b0aa2373f44a1612
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 03:08:21 +0100

    add bell-icon to header component

--- src/pages/dashboard/components/Header.js
+++ src/pages/dashboard/components/Header.js
@@ -1,6 +1,6 @@
 import { Avatar, AvatarBadge, FormControl, Box, HStack, Icon, Image, Input, InputGroup, InputLeftElement, Spacer, Switch, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
-import { Search2Icon } from "@chakra-ui/icons"
+import { Search2Icon, BellIcon } from "@chakra-ui/icons"
 
 
 import user from '../../../assets/images/users/mira.jpg'
@@ -22,12 +22,10 @@ const Header = ()=> {
 
             <HStack>
                 <Text fontWeight="700">SC</Text>
-                <FormControl width="fit-content" zIndex="1">
-                    <Switch id='email-alerts' />
-                </FormControl>
+                <Switch id='email-alerts' />
                 <Text fontWeight="700">NGN</Text>
                 <Spacer /><Spacer />
-                <Icon name="notification" />
+                <BellIcon />
                 <Spacer />
 
                 <UserAvatar avatar={ user } name="Mirabel Watson" />
