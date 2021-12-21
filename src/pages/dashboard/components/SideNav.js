5cda6ac9886a9a32d8a3efe8d2591f8ce45a9483
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 03:17:47 +0100

    add tailwindcss fixed, left-0, top-0, h-full, bg-white utility classes to side-nav component

--- src/pages/dashboard/components/SideNav.js
+++ src/pages/dashboard/components/SideNav.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SideNav = ()=> {
     return (
-        <aside className="aside_nav">
+        <aside className="aside_nav fixed left-0 top-0 h-full bg-white">
 
         </aside>
     )
