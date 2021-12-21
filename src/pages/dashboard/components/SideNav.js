f09a619eb717fc64b7de1097ea68bf0dacaa5c99
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 14:21:23 +0100

    add icons for when a tab is active

--- src/pages/dashboard/components/SideNav.js
+++ src/pages/dashboard/components/SideNav.js
@@ -4,23 +4,34 @@ import React, { useState } from 'react'
 
 import hideaLogo from "../../../assets/images/logo/Hidea-logo.svg"
 
+
+//Linear icons
 import DashboardIcon from '../../../assets/images/icons/icons-set/linear/category.svg'
-import SettingIcon from '../../../assets/images/icons/icons-set/linear/setting-2.svg'
+import SettingsIcon from '../../../assets/images/icons/icons-set/linear/setting-2.svg'
 import WalletIcon from '../../../assets/images/icons/icons-set/linear/wallet.svg'
 import IdeaPooolIcon from '../../../assets/images/icons/icons-set/linear/box.svg'
 import SupportIcon from '../../../assets/images/icons/icons-set/linear/device-message.svg'
 import MyIdeasIcon from '../../../assets/images/icons/icons-set/linear/lock.svg'
 
 
+//BoldIcons
+import DashboardIconBold from '../../../assets/images/icons/icons-set/bold/category.svg'
+import SettingsIconBold from '../../../assets/images/icons/icons-set/bold/setting-2.svg'
+import WalletIconBold from '../../../assets/images/icons/icons-set/bold/wallet.svg'
+import IdeaPooolIconBold from '../../../assets/images/icons/icons-set/bold/box.svg'
+import SupportIconBold from '../../../assets/images/icons/icons-set/bold/device-message.svg'
+import MyIdeasIconBold from '../../../assets/images/icons/icons-set/bold/lock.svg'
+
+
 const SideNav = ()=> {
 
     const [tabItems, setTabItems] = useState([
-        {text: "Dashboard", TabIcon: DashboardIcon, active: true},
-        {text: "My ideas", TabIcon: MyIdeasIcon, active: false},
-        {text: "Ideas pool", TabIcon: IdeaPooolIcon, active: false},
-        {text: "Wallet", TabIcon: AddIcon, active: false},
-        {text: "Settings", TabIcon: AddIcon, active: false},
-        {text: "Support", TabIcon: AddIcon, active: false}
+        {text: "Dashboard", TabIcon: DashboardIcon, TabIconActive: DashboardIconBold, active: true},
+        {text: "My ideas", TabIcon: MyIdeasIcon, TabIconActive: MyIdeasIconBold, active: false},
+        {text: "Ideas pool", TabIcon: IdeaPooolIcon, TabIconActive: IdeaPooolIconBold, active: false},
+        {text: "Wallet", TabIcon: WalletIcon, TabIconActive: WalletIconBold, active: false},
+        {text: "Settings", TabIcon: SettingsIcon, TabIconActive: SettingsIconBold, active: false},
+        {text: "Support", TabIcon: SupportIcon, TabIconActive: SupportIconBold, active: false}
     ]);
 
 
@@ -47,7 +58,7 @@ const SideNav = ()=> {
                 {
                     tabItems.map(({ TabIcon, text, active }, idx) => (
                         <ListItem key={idx} onClick={()=> changeActiveTab(idx)} padding="4" pl="8" cursor="pointer" display="flex" borderRadius="8px" borderTopLeftRadius="0" borderBottomLeftRadius="0" alignItems="center" bg={active ? "#73DA9E" : null} color={active ? "#fff" : null}>
-                            { <ListIcon as={TabIcon} mr="18px" /> }
+                            { <Image src={ TabIcon } alt={ text } mr="18px"/> }
                             { text }
                         </ListItem>
                     ))
