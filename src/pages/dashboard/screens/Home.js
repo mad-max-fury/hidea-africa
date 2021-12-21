fc287aeca1f8444e3430d7e336b307cd1cd53143
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 08:56:54 +0100

    wrap dashboard home screen contents with chakra-ui Box component

--- src/pages/dashboard/screens/Home.js
+++ src/pages/dashboard/screens/Home.js
@@ -1,10 +1,11 @@
+import { Box, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Home = ()=> {
     return (
-        <div>
-            
-        </div>
+        <Box>
+            <Text>Welcome Allison,</Text>
+        </Box>
     )
 }
 
