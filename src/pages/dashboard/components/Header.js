259be662dbad09c1a28a1ef05e18b79efbaa47db
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-21 02:11:36 +0100

    add semactive header elements to Header component for dashboard page

--- src/pages/dashboard/components/Header.js
+++ src/pages/dashboard/components/Header.js
@@ -1,10 +1,11 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const Header = ()=> {
     return (
-        <div>
+        <header>
             
-        </div>
+        </header>
     )
 }
 
