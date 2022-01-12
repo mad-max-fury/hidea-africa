428ca784ff132ba4793ac50d5c5847a0b5b41b1b
tochukwujonah <jonahtochukwu@gmail.com>
2022-01-12 08:20:48 +0100

    updated the IdeaCard component

--- src/pages/dashboard/components/IdeaCard.js
+++ src/pages/dashboard/components/IdeaCard.js
@@ -1,18 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Avatar, Box, Text, VStack, HStack, } from '@chakra-ui/react'
 
 
-const IdeaCard = ({ image, seller, tag, invState, percentage, isSelected=false })=> {
+const IdeaCard = ({ idx, image, seller, tag, invState, percentage, isSelected=false, ideas, setIdeas, setActive })=> {
     const [selected, setSelected] = useState(isSelected);
 
-    const clickHandler = ()=> setSelected(true);
+    const clickHandler = ()=> {
+        const ideas_copy = [...ideas.map(idea => {
+            idea.isSelected = false
+            return idea
+        })];
+
+        ideas_copy[idx].isSelected = true;
+        setActive(idx);
+
+        setIdeas(ideas_copy);
+    }
+
+
+    useEffect(()=> {
+        setSelected(isSelected);
+    }, [isSelected])
 
     return (
         <Box display="flex" alignItems="center" justifyContent="space-between" padding="4" w="100%" h="82px" bg={ selected ? "#878696" : "#fff"} borderRadius="16px" onClick={ clickHandler }>
             <HStack>
                 <Avatar src={ image } borderRadius="8px" w="50px" h="50px" mr="4" />
                 <VStack mr="50px" alignItems="flex-start">
-                    <Text color={ selected ? "#fff" : "#57575b" } fontWeight="700">{ seller.name }</Text>
+                    <Text color={ selected ? "#fff" : "#57575b" } fontWeight="700">{ seller.seller_name }</Text>
                     <Text color={ selected ? "#fff" : "#5F5E73" } fontSize="14px">{ tag }</Text>
                 </VStack>
             </HStack>
@@ -27,12 +42,12 @@ const IdeaCard = ({ image, seller, tag, invState, percentage, isSelected=false }
 }
 
 export const Cards = ({children}) =>{
+    
     return (
 
-        <>
-            <h1>Cards Display</h1>
+        <Box w="full" mt="10px">
             {children}
-        </>
+        </Box>
     )
 }
 
