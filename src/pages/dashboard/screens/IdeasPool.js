13440f6f81c517440a60fc5a22895c1082f37bbe
mad-max-fury <chrisnonso8@gmail.com>
2022-03-05 17:55:14 +0000

    entrepreneur screens
    entrepreneur screens

--- src/pages/dashboard/screens/IdeasPool.js
+++ src/pages/dashboard/screens/IdeasPool.js
@@ -1,13 +1,13 @@
 import { Avatar, Box, Button, Flex, Grid, Heading, Text, VStack, Input, InputGroup, InputLeftElement, Stack, Spacer, Select, HStack, Image, CloseButton, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Drop from '../../../assets/images/icons/icons-set/bold/drop.svg'
 import Techy from '../../../assets/images/techy.jpg'
 import { ideasModel } from '../../../model/ideas.model.js'
 
-const IdeasPool = ()=> {
+const IdeasPool = () => {
     return (
 
-=======
+
         <Box width="full" direction="row">
             <Flex width='full' justifyContent="space-between">
                 <VStack bg='#fff' width='25%'>
@@ -15,26 +15,26 @@ const IdeasPool = ()=> {
 
                         {/* Investment selection options */}
 
-                        <Select bg='#fff' mb="20px" icon= {<Image src = {Drop}/>} variant='filled'>
+                        <Select bg='#fff' mb="20px" icon={<Image src={Drop} />} variant='filled'>
                             <option value="A-Z">Investment amount</option>
                             <option value="A-Z">N 50,000</option>
                             <option value="A-Z">N 100,000</option>
                             <option value="A-Z">N 150,000</option>
                             <option value="A-Z">N 250,000</option>
-                        
-                            <option value="A-Z">N 500,000</option>                
-                        </Select>   
-                        <Select bg='#fff' mb="20px" icon= {<Image src = {Drop}/>} variant='filled'>
+
+                            <option value="A-Z">N 500,000</option>
+                        </Select>
+                        <Select bg='#fff' mb="20px" icon={<Image src={Drop} />} variant='filled'>
                             <option value="A-Z">Category</option>
-                            
-                        </Select>   
-                        <Select bg='#fff' icon= {<Image src = {Drop}/>} variant='filled'>
+
+                        </Select>
+                        <Select bg='#fff' icon={<Image src={Drop} />} variant='filled'>
                             <option value="A-Z">ROI</option>
-                            
-                        </Select>    
+
+                        </Select>
                     </Box>
                 </VStack>
-                
+
                 <Stack w="70%" >
                     <HStack w="full" justifyContent="space-between">
                         <Text fontWeight="black">Recommended for you</Text>
@@ -44,32 +44,32 @@ const IdeasPool = ()=> {
                     {/* IdeaPool ideacards */}
                     <HStack>
                         <IdeaCardType />
-                        
+
                     </HStack>
                 </Stack>
-                
+
             </Flex>
 
-           
 
-        </Box>               
+
+        </Box>
     )
 }
 
-const IdeaCardType = ()=> {
+const IdeaCardType = () => {
     const [ideas, setIdeas] = useState(ideasModel);
     return (
         <Box display="flex" h="40vh" w="100%">
             <VStack w="40%" h="40vh">
-                <Image borderRadius="16px" height="30%" src= {Techy} />
+                <Image borderRadius="16px" height="30%" src={Techy} />
                 <Text>{ideas.sector}</Text>
             </VStack>
             <VStack w="40%" h="40vh">
-                <Image borderRadius="16px" height="30%" src= {Techy} />
+                <Image borderRadius="16px" height="30%" src={Techy} />
                 <Text>{ideas.sector}</Text>
             </VStack>
-           
-            
+
+
         </Box>
     )
 
--- src/pages/enterpreneur/dashboard/components/projectIdeaCard.js
+++ src/pages/enterpreneur/dashboard/components/projectIdeaCard.js
@@ -1,17 +1,136 @@
 import React, { useState, useEffect } from 'react'
-import { Avatar, Box, Text, VStack, HStack, Heading, } from '@chakra-ui/react'
+import { Avatar, Box, Text, VStack, HStack, Heading, useDisclosure, Button, Flex, Image, } from '@chakra-ui/react'
 import TopCardlayer from './TopCardlayer';
 import BottomCardLayer from './BottomCardLayer';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
+import AfenaAvatar from '../../../../assets/images/users/afena.jpg'
+
 
 
 const ProjectIdeaCard = ({ draft, Active, status }) => {
 
-    return (
-        <Box bg={'#ffffff'} cursor="pointer" display="flex" flexDirection={'column'} alignItems="start" gap={'1rem'} style={{ boxShadow: '0px 0px 4px #f5f5f7' }} padding="4" w="335px" h="fit-content"  >
-            <TopCardlayer draft={draft} />
-            <BottomCardLayer draft={draft} Active={Active} status={status} />
-        </Box>
 
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <>
+
+            <Box bg={'#ffffff'} cursor="pointer" display="flex" flexDirection={'column'} alignItems="start" gap={'1rem'} style={{ boxShadow: '0px 0px 4px #f5f5f7' }} padding="4" w="335px" h="fit-content" onClick={onOpen}>
+                <TopCardlayer draft={draft} />
+                <BottomCardLayer draft={draft} Active={Active} status={status} />
+            </Box>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent maxW={'40vw'} maxH='fit-content'>
+                    <ModalHeader></ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody display={'flex'} width='85%' margin={'auto'} flexDirection='column' gap={'4'}>
+                        <Box width="full" display="flex" flexDirection="row" alignItems="center" justifyContent={'space-between'}
+                            padding='1rem 0'>
+                            <Box display={'flex'} gap='1rem' alignItems={'center'}>
+                                <Avatar size='md' borderRadius={'md'} name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
+                                <Box>
+                                    <Heading as={'h3'} fontSize='30px' lineHeight={'24px'}>Avatar republic</Heading>
+
+                                </Box>
+                            </Box>
+                            <Box display={'flex'} gap='.2rem' _hover={{ transition: 'all .1s ease-in', gap: '.3rem' }}>
+                                <Button alignSelf={'flex-end'} bgColor={status === 'In-Progress' ? '#FEF8E6' : status === 'Completed' ? 'green.100' : 'red.100'} textColor={status === 'In-Progress' ? '#7A6003' : status === 'Completed' ? 'green.500' : 'red'} borderRadius='25px'>{status === 'In-Progress' ? 'In-Progress' : status === 'Completed' ? 'Completed' : 'Cancelled'}</Button>
+
+                            </Box>
+                        </Box>
+                        <Box border={'1px'} height='8rem' borderRadius={'10px'}> </Box>
+                        <Box width={'full'}>
+                            <Text textColor={'#000'} fontWeight='bolder'>Description</Text>
+                            <span style={{ fontSize: '14px' }} textColor={'grey'}>Bridging gap between farmers and  consumers by providing a platform for produce exchange while acting as the middle man between both parties</span>
+                        </Box>
+                        <Box display={'flex'} flexDirection='row' justifyContent={'space-between'}>
+                            <Box display={'flex'} w='50%' flexDirection={'column'} gap='4' >
+                                <Box>
+                                    <Heading as='h3' fontSize='16px'>Amount</Heading>
+                                    <Text textColor={'GrayText'}>SC 35,000,000 </Text>
+                                </Box>
+                                <Box>
+                                    <Heading as='h3' fontSize='16px'>Category</Heading>
+                                    <Text textColor={'GrayText'}>Fintech, Agriculture</Text>
+                                </Box>
+                                <Box>
+                                    <Heading as='h3' fontSize='16px'>Investment duration</Heading>
+                                    <Text textColor={'GrayText'}>2 years</Text>
+                                </Box>
+                            </Box>
+                            <Box display={'flex'} w='50%' flexDirection={'column'} gap='4'>
+                                <Box>
+                                    <Heading as='h3' fontSize='16px'>Investor</Heading>
+                                    <Text textColor={'green'}> Allison Uwa</Text>
+                                </Box>
+                                <Box>
+                                    <Heading as='h3' fontSize='16px'> Return on Investment (ROI)</Heading>
+                                    <Text textColor={'GrayText'}>5% Equity</Text>
+                                </Box>
+                                <Box>
+                                    <Heading as='h3' fontSize='16px'>Maturity date</Heading>
+                                    <Text textColor={'GrayText'}>12 July, 2027 </Text>
+                                </Box>
+                            </Box>
+
+                        </Box>
+                        <VStack w="full" alignItems="left">
+
+                            <Text color="#000">Team Members</Text>
+
+
+                            <Flex alignItems="left" w="full">
+
+                                <VStack w="100%" alignItems="center">
+                                    <Avatar size='md' borderRadius={'md'} name='Dan Abrahmov' src={AfenaAvatar} />
+
+                                    <Text color="secondary.100">Olumide Francis</Text>
+                                    <Text color="#57575B" fontSize="14px">CEO</Text>
+                                </VStack>
+
+                                <VStack w="100%" alignItems="center">
+                                    <Avatar size='md' borderRadius={'md'} name='Dan Abrahmov' src={AfenaAvatar} />
+
+                                    <Text color="secondary.100">Asabi Francis</Text>
+                                    <Text color="#57575B" fontSize="14px">Co-founder</Text>
+                                </VStack>
+
+                                <VStack w="100%" alignItems="center">
+                                    <Avatar size='md' borderRadius={'md'} name='Dan Abrahmov' src={AfenaAvatar} />
+
+                                    <Text color="secondary.100">Kudirat Bakare</Text>
+                                    <Text color="#57575B" fontSize="14px">Customer Service</Text>
+                                </VStack>
+
+                                <VStack w="100%" alignItems="center">
+                                    <Avatar size='md' borderRadius={'md'} name='Dan Abrahmov' src={AfenaAvatar} />
+
+                                    <Text color="secondary.100">Alex nwofor</Text>
+                                    <Text color="#57575B" fontSize="14px">Marketing</Text>
+                                </VStack>
+                            </Flex>
+                        </VStack>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+
+        </>
     )
 }
 
