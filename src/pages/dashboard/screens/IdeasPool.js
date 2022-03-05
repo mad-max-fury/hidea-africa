c28c248799c91ca55045ae7eed7d39db4c49f10e
tochukwujonah <jonahtochukwu@gmail.com>
2022-03-05 13:57:58 +0100

    updated the ideapool screens

--- src/pages/dashboard/screens/IdeasPool.js
+++ src/pages/dashboard/screens/IdeasPool.js
@@ -1,25 +1,27 @@
 import { Avatar, Box, Button, Flex, Grid, Heading, Text, VStack, Input, InputGroup, InputLeftElement, Stack, Spacer, Select, HStack, Image, CloseButton, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import React from 'react'
+import React, {useState} from 'react'
 import Drop from '../../../assets/images/icons/icons-set/bold/drop.svg'
+import Techy from '../../../assets/images/techy.jpg'
+import { ideasModel } from '../../../model/ideas.model.js'
 
 const IdeasPool = ()=> {
     return (
-        <Box width="full" height="100vh" direction="row">
-            <Flex width='full'>
-                <VStack bg='#fff' width='20%' mt='20px'>
-                    <Box>
-                        <Select bg='#fff' icon= {<Image src = {Drop}/>} variant='filled'>
-                            <option value="A-Z">Investment Amount</option>
+        <Box width="full" direction="row">
+            <Flex width='full' justifyContent="space-between">
+                <VStack bg='#fff' width='25%'>
+                    <Box w="full" p="1rem">
+
+                        {/* Investment selection options */}
+
+                        <Select bg='#fff' mb="20px" icon= {<Image src = {Drop}/>} variant='filled'>
+                            <option value="A-Z">Investment amount</option>
                             <option value="A-Z">N 50,000</option>
                             <option value="A-Z">N 100,000</option>
                             <option value="A-Z">N 150,000</option>
                             <option value="A-Z">N 250,000</option>
-                            <option value="A-Z">N 500,000</option>
-                            
-                            
-                            
+                            <option value="A-Z">N 500,000</option>                
                         </Select>   
-                        <Select bg='#fff' icon= {<Image src = {Drop}/>} variant='filled'>
+                        <Select bg='#fff' mb="20px" icon= {<Image src = {Drop}/>} variant='filled'>
                             <option value="A-Z">Category</option>
                             
                         </Select>   
@@ -29,14 +31,45 @@ const IdeasPool = ()=> {
                         </Select>    
                     </Box>
                 </VStack>
+                
+                <Stack w="70%" >
+                    <HStack w="full" justifyContent="space-between">
+                        <Text fontWeight="black">Recommended for you</Text>
+                        <Text color="secondary.100" cursor="pointer">View All</Text>
+                    </HStack>
 
-                <HStack>
-
-                </HStack>
+                    {/* IdeaPool ideacards */}
+                    <HStack>
+                        <IdeaCardType />
+                        
+                    </HStack>
+                </Stack>
+                
             </Flex>
 
-        </Box>        
+            
+
+        </Box>               
     )
+}
+
+const IdeaCardType = ()=> {
+    const [ideas, setIdeas] = useState(ideasModel);
+    return (
+        <Box display="flex" h="40vh" w="100%">
+            <VStack w="40%" h="40vh">
+                <Image borderRadius="16px" height="30%" src= {Techy} />
+                <Text>{ideas.sector}</Text>
+            </VStack>
+            <VStack w="40%" h="40vh">
+                <Image borderRadius="16px" height="30%" src= {Techy} />
+                <Text>{ideas.sector}</Text>
+            </VStack>
+           
+            
+        </Box>
+    )
+
 }
 
 export default IdeasPool
